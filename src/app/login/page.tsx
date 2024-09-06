982c46a47e1240f313b3750499c7d64f6a18e4ef
oliullah05 <mdolihasan@gmail.com>
2024-09-06 16:18:09 +0600

    all input deleted

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,17 +5,9 @@ const page = () => {
             <h1>hi This is login page</h1>
 
             <form >
-                <input type="text" name="name" id="" />
-                <input type="text" name="password" id="" />
-                <input type="password" name="" id="" />
-                <input type="password" name="" id="" /> 
-                <input type="tel" name="" id="" />
-                <input type="button" name="" id="" />
-               <input type="button" value="oli" />
-               <input type="button" value="2" />
-               <input type="button" value="3" />
+                <h1>This is h1 </h1>
             </form>
-            
+
         </>
     );
 };
