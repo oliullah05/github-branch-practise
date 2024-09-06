2fc37c2ac3da8b8730164150261bad8462198852
oliullah05 <mdolihasan@gmail.com>
2024-09-06 16:00:13 +0600

    three button added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,10 +7,12 @@ const page = () => {
             <form >
                 <input type="text" name="name" id="" />
                 <input type="text" name="password" id="" />
-                <input type="password" name="" id="" /> 
+                <input type="password" name="" id="" />
                 <input type="tel" name="" id="" />
                 <input type="button" name="" id="" />
-
+               <input type="button" value="oli" />
+               <input type="button" value="2" />
+               <input type="button" value="3" />
             </form>
         </>
     );
