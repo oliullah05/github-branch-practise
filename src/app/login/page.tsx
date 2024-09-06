6ccebdc9eb835ac28c1f192feb7e3f6bfbd1e94f
oliullah05 <mdolihasan@gmail.com>
2024-09-06 23:56:48 +0600

    created new pull from dev-1

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+
+const page = () => {
+    return (
+        <div>
+            <form >
+               <input type="text" value="" />
+               <input type="text" value="" />
+               <button type="submit">Login here</button>
+            </form>
+        </div>
+    );
+};
+
+export default page;
